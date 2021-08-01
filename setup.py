f011fd19e8baa37be2b9df280a14054cb91aec22
Nex <nex@nex.sx>
2021-08-01 21:11:08 +0200

    More explicit copyright and licensing notes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # Mobile Verification Toolkit (MVT)
-# Copyright (c) 2021 MVT Project Developers.
-# See the file 'LICENSE' for usage and copying permissions, or find a copy at
-#   https://github.com/mvt-project/mvt/blob/main/LICENSE
+# Copyright (c) 2021 The MVT Project Authors.
+# Use of this software is governed by the MVT License 1.1 that can be found at
+#   https://license.mvt.re/1.1/
 
 import os
 
