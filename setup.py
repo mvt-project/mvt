d43c8109d120e853658be4ae37a4aba0028186a3
Nex <nex@nex.sx>
2021-08-25 16:32:05 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 from setuptools import find_packages, setup
 
 __package_name__ = "mvt"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __description__ = "Mobile Verification Toolkit"
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
