94e3c0ce7b05c1f170ba96a2056aaedc993f8d3f
Nex <nex@nex.sx>
2021-09-22 17:27:29 +0200

    Added iOS 15.0

--- mvt/ios/versions.py
+++ mvt/ios/versions.py
@@ -223,6 +223,7 @@ IPHONE_IOS_VERSIONS = [
     {"build": "18G69", "version": "14.7"},
     {"build": "18G82", "version": "14.7.1"},
     {"build": "18H17", "version": "14.8"},
+    {"build": "19A346", "version": "15.0"},
 ]
 
 def get_device_desc_from_id(identifier, devices_list=IPHONE_MODELS):
