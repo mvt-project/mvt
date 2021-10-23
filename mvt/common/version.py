6d8de5b461df05dbd387566bb496a6e95c2f6795
Nex <nex@nex.sx>
2021-10-23 13:51:44 +0200

    Bumped version

--- mvt/common/version.py
+++ mvt/common/version.py
@@ -6,7 +6,7 @@
 import requests
 from packaging import version
 
-MVT_VERSION = "1.2.12"
+MVT_VERSION = "1.2.13"
 
 def check_for_updates():
     res = requests.get("https://pypi.org/pypi/mvt/json")
