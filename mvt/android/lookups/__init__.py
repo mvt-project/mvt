eb5f07a75dae99ea10133d44f4bd8ce8a928eb88
Nex <nex@nex.sx>
2022-01-30 20:15:01 +0100

    Updated copyright notice

--- mvt/android/lookups/__init__.py
+++ mvt/android/lookups/__init__.py
@@ -1,4 +1,4 @@
 # Mobile Verification Toolkit (MVT)
-# Copyright (c) 2021 The MVT Project Authors.
+# Copyright (c) 2021-2022 The MVT Project Authors.
 # Use of this software is governed by the MVT License 1.1 that can be found at
 #   https://license.mvt.re/1.1/
