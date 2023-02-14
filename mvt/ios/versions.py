8ce6b3129999289ad46461abcd9475754c496fc2
tek <tek@randhome.io>
2023-02-13 19:21:41 -0500

    Adding latest iOS version 16.3.1

--- mvt/ios/versions.py
+++ mvt/ios/versions.py
@@ -254,7 +254,8 @@ IPHONE_IOS_VERSIONS = [
     {"build": "20B101", "version": "16.1.1"},
     {"build": "20B110", "version": "16.1.2"},
     {"build": "20C65", "version": "16.2"},
-    {"build": "20D47", "version": "16.3"}
+    {"build": "20D47", "version": "16.3"},
+    {"build": "20D67", "version": "16.3.1"}
 ]
 
 
