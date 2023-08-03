b259db30f85e4bc9079809c6e1a3faf94f1e0ae9
Nex <nex@nex.sx>
2023-08-03 08:06:59 +0200

    Added missing empty lines

--- mvt/android/artifacts/dumpsys_dbinfo.py
+++ mvt/android/artifacts/dumpsys_dbinfo.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2021-2023 Claudio Guarnieri.
 # Use of this software is governed by the MVT License 1.1 that can be found at
 #   https://license.mvt.re/1.1/
+
 import re
 
 from .artifact import AndroidArtifact
