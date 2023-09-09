9988887d2704184660afd734696508a3ee1f6221
Nex <nex@nex.sx>
2023-09-09 17:55:27 +0200

    Updated copyright notice

--- mvt/android/modules/androidqf/sms.py
+++ mvt/android/modules/androidqf/sms.py
@@ -1,5 +1,5 @@
 # Mobile Verification Toolkit (MVT)
-# Copyright (c) 2021-2023 Claudio Guarnieri.
+# Copyright (c) 2021-2023 The MVT Authors.
 # Use of this software is governed by the MVT License 1.1 that can be found at
 #   https://license.mvt.re/1.1
 
