mvt-project/mvt

commit 81b647beac4ec56e4462d504b48a6aa06c09d4b6

Add basic support for IP indicators in MVT (#556)
* Add prelimary ipv4-addr ioc matching support under collection domains

* Add IP addresses as a valid IOC type

This currently just supports IPv4 addresses which
are treated as domains internally in MVT.

---------

Co-authored-by: renini <renini@local>

src/mvt/common/indicators.py
@@ -107,6 +107,13 @@ class Indicators:
                 ioc_coll=collection,
                 ioc_coll_list=collection["domains"],
             )
+        if key == "ipv4-addr:value":
+            # We treat IP addresses as simple domains here to ease checks.
+            self._add_indicator(
+                ioc=value.strip(),
+                ioc_coll=collection,
+                ioc_coll_list=collection["domains"],
+            )
         elif key == "process:name":
             self._add_indicator(
                 ioc=value, ioc_coll=collection, ioc_coll_list=collection["processes"]

tests/artifacts/generate_stix.py
@@ -13,6 +13,7 @@ def generate_test_stix_file(file_path):
         os.remove(file_path)
 
     domains = ["example.org"]
+    ip_addresses = ["198.51.100.1"]
     processes = ["Launch"]
     emails = ["foobar@example.org"]
     filenames = ["/var/foobar/txt"]
@@ -28,6 +29,15 @@ def generate_test_stix_file(file_path):
         i = Indicator(
             indicator_types=["malicious-activity"],
             pattern="[domain-name:value='{}']".format(d),
+            pattern_type="stix",
+        )
+        res.append(i)
+        res.append(Relationship(i, "indicates", malware))
+
+    for a in ip_addresses:
+        i = Indicator(
+            indicator_types=["malicious-activity"],
+            pattern="[ipv4-addr:value='{}']".format(a),
             pattern_type="stix",
         )
         res.append(i)

tests/common/test_indicators.py
@@ -15,8 +15,8 @@ class TestIndicators:
         ind = Indicators(log=logging)
         ind.load_indicators_files([indicator_file], load_default=False)
         assert len(ind.ioc_collections) == 1
-        assert ind.ioc_collections[0]["count"] == 8
-        assert len(ind.ioc_collections[0]["domains"]) == 1
+        assert ind.ioc_collections[0]["count"] == 9
+        assert len(ind.ioc_collections[0]["domains"]) == 2
         assert len(ind.ioc_collections[0]["emails"]) == 1
         assert len(ind.ioc_collections[0]["file_names"]) == 1
         assert len(ind.ioc_collections[0]["processes"]) == 1
@@ -74,6 +74,10 @@ class TestIndicators:
         assert ind.check_url("https://github.com") is None
         assert ind.check_url("https://example.com/") is None
 
+        # Test detecting IP address indicators from STIX.
+        assert ind.check_url("https://198.51.100.1:8080/")
+        assert ind.check_url("https://1.1.1.1/") is None
+
     def test_check_file_hash(self, indicator_file):
         ind = Indicators(log=logging)
         ind.load_indicators_files([indicator_file], load_default=False)
@@ -98,4 +102,4 @@ class TestIndicators:
         os.environ["MVT_STIX2"] = indicator_file
         ind = Indicators(log=logging)
         ind.load_indicators_files([], load_default=False)
-        assert ind.total_ioc_count == 8
+        assert ind.total_ioc_count == 9

commit 3afe218c7cb91898d7b513a2c265d39e6c2f71cd

Add support for check APK certificate hash IOCs (#557)
* Fix bug loading indicators which I introduced in 81b647b

* Add support for matching on APK certificate hash IOCs

src/mvt/android/modules/androidqf/packages.py
@@ -77,13 +77,24 @@ class Packages(AndroidQFModule):
             if ioc:
                 result["matched_indicator"] = ioc
                 self.detected.append(result)
-                continue
 
             for package_file in result.get("files", []):
                 ioc = self.indicators.check_file_hash(package_file["sha256"])
                 if ioc:
                     result["matched_indicator"] = ioc
                     self.detected.append(result)
+
+                if "certificate" not in package_file:
+                    continue
+
+                # The keys generated by AndroidQF have a leading uppercase character
+                for hash_type in ["Md5", "Sha1", "Sha256"]:
+                    certificate_hash = package_file["certificate"][hash_type]
+                    ioc = self.indicators.check_app_certificate_hash(certificate_hash)
+                    if ioc:
+                        result["matched_indicator"] = ioc
+                        self.detected.append(result)
+                        break
 
     def run(self) -> None:
         packages = self._get_files_by_pattern("*/packages.json")

src/mvt/common/indicators.py
@@ -82,6 +82,7 @@ class Indicators:
             "files_md5": [],
             "files_sha1": [],
             "files_sha256": [],
+            "app_cert_hashes": [],
             "app_ids": [],
             "ios_profile_ids": [],
             "android_property_names": [],
@@ -107,7 +108,7 @@ class Indicators:
                 ioc_coll=collection,
                 ioc_coll_list=collection["domains"],
             )
-        if key == "ipv4-addr:value":
+        elif key == "ipv4-addr:value":
             # We treat IP addresses as simple domains here to ease checks.
             self._add_indicator(
                 ioc=value.strip(),
@@ -145,6 +146,24 @@ class Indicators:
             self._add_indicator(
                 ioc=value, ioc_coll=collection, ioc_coll_list=collection["files_sha256"]
             )
+        elif key == "app:cert.md5":
+            self._add_indicator(
+                ioc=value,
+                ioc_coll=collection,
+                ioc_coll_list=collection["app_cert_hashes"],
+            )
+        elif key == "app:cert.sha1":
+            self._add_indicator(
+                ioc=value,
+                ioc_coll=collection,
+                ioc_coll_list=collection["app_cert_hashes"],
+            )
+        elif key == "app:cert.sha256":
+            self._add_indicator(
+                ioc=value,
+                ioc_coll=collection,
+                ioc_coll_list=collection["app_cert_hashes"],
+            )
         elif key == "app:id":
             self._add_indicator(
                 ioc=value, ioc_coll=collection, ioc_coll_list=collection["app_ids"]
@@ -155,7 +174,6 @@ class Indicators:
                 ioc_coll=collection,
                 ioc_coll_list=collection["ios_profile_ids"],
             )
-
         elif key == "android-property:name":
             self._add_indicator(
                 ioc=value,
@@ -697,6 +715,29 @@ class Indicators:
                     'Found a known suspicious file with hash "%s" '
                     'matching indicators from "%s"',
                     file_hash,
+                    ioc["name"],
+                )
+                return ioc
+
+        return None
+
+    def check_app_certificate_hash(self, cert_hash: str) -> Union[dict, None]:
+        """Check the provided cert hash against the list of indicators.
+
+        :param cert_hash: hash to check
+        :type cert_hash: str
+        :returns: Indicator details if matched, otherwise None
+
+        """
+        if not cert_hash:
+            return None
+
+        for ioc in self.get_iocs("app_cert_hashes"):
+            if cert_hash.lower() == ioc["value"].lower():
+                self.log.warning(
+                    'Found a known suspicious app certfificate with hash "%s" '
+                    'matching indicators from "%s"',
+                    cert_hash,
                     ioc["name"],
                 )
                 return ioc

tests/android_androidqf/test_packages.py
@@ -86,3 +86,19 @@ class TestAndroidqfPackages:
             module.detected[0]["matched_indicator"]["value"]
             == "31037a27af59d4914906c01ad14a318eee2f3e31d48da8954dca62a99174e3fa"
         )
+
+    def test_packages_certificate_hash_ioc(self, module, indicators_factory):
+        module.indicators = indicators_factory(
+            app_cert_hashes=[
+                "c7e56178748be1441370416d4c10e34817ea0c961eb636c8e9d98e0fd79bf730"
+            ]
+        )
+
+        run_module(module)
+
+        assert len(module.detected) == 1
+        assert module.detected[0]["name"] == "com.malware.muahaha"
+        assert (
+            module.detected[0]["matched_indicator"]["value"]
+            == "c7e56178748be1441370416d4c10e34817ea0c961eb636c8e9d98e0fd79bf730"
+        )

tests/conftest.py
@@ -37,6 +37,7 @@ def indicators_factory(indicator_file):
         file_names=[],
         processes=[],
         app_ids=[],
+        app_cert_hashes=[],
         android_property_names=[],
         files_sha256=[],
     ):
@@ -50,6 +51,7 @@ def indicators_factory(indicator_file):
         ind.ioc_collections[0]["app_ids"].extend(app_ids)
         ind.ioc_collections[0]["android_property_names"].extend(android_property_names)
         ind.ioc_collections[0]["files_sha256"].extend(files_sha256)
+        ind.ioc_collections[0]["app_cert_hashes"].extend(app_cert_hashes)
 
         return ind
 